90f77a24db3d636198b7ea2e64f71ea0f177411d
fwosner <fwosner@gmail.com>
2019-01-22 22:00:19 -0300

    Archivos y prácticas Clase 3

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* Mini instroduccion a JavaScript:
+Funciones y eventos */
+
+function saludar(){ // <<-- Operación lógica
+	window.alert("Hola desde JavaScript")
+}
+
+document.querySelector("h1").onclick = saludar; //<<-- Evento/Comportamiento...
